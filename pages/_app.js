7cd8aae167190d53312e5d321c11735181f9fdd8
igoror12 <igoror11@hotmail.com>
2022-03-10 21:07:35 +0000

    Alterações com GoogleAnalytics(code)

--- pages/_app.js
+++ pages/_app.js
@@ -5,8 +5,22 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                {/*<!-- Global site tag (gtag.js) - Google Analytics -->*/}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-DV3K4XXSJK"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html:`
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-DV3K4XXSJK');
+                        `
+                    }}        
+                />
+                
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin={true.toString()} />
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap" rel="stylesheet" />
             </Head>
             <GlobalStyle />
