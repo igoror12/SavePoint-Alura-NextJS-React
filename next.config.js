0550ee1fddcc020db90a5297cc8e34309c3eaf94
igoror12 <igoror11@hotmail.com>
2022-03-10 18:29:49 +0000

    first commit

--- next.config.js
+++ next.config.js
@@ -4,3 +4,15 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/perguntas',
+        destination: '/faq',
+        permanent: true,
+      },
+    ]
+  },
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from 'next/head';
+import GlobalStyle from '../src/theme/GlobalStyle';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@800&display=swap" rel="stylesheet" />
+            </Head>
+            <GlobalStyle />
+            <Component {...pageProps} />
+        </>
+    )
 }
 
-export default MyApp
+export default MyApp;
